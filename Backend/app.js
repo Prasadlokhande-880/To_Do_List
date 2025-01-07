b1776ac65a40a36527cad59905bd62284e7821b2
YourGitHubUserName <118651899+Prasadlokhnade@users.noreply.github.com>
2025-01-08 00:13:51 +0530

    Create a New React App

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(express.json());
 
 // Task structure class
 class TaskStructure {
